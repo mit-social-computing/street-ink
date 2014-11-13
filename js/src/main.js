176abdc838aa7b9657c6bcde7c1529ac013e355e
Brian Whitton <me@brianwhitton.com>
2014-11-13 00:22:56 -0500

    line segments changing color during drawing

--- js/src/main.js
+++ js/src/main.js
@@ -6,16 +6,42 @@ function $(elementID) {
 
 function mousemove(ops) {
     distance += Math.sqrt(Math.pow(ops.e.movementX, 2) + Math.pow(ops.e.movementY, 2))
-    console.log('distance:', distance )
-    console.log( 'street length:', parseFloat(data[currentCity].length/100) )
-    if ( distance > parseFloat(data[currentCity].length/100) ) {
-        if ( colorIndex > colors.length - 1 ) { colorIndex = 0 }
-        c.freeDrawingBrush._finalizeAndAddPath()
-        colorIndex++
-        c.freeDrawingBrush.color = colors[colorIndex]
 
-        distance -= parseFloat(data[currentCity].length/100)
-        currentCity++
+    try {
+        if ( distance * 10 > parseFloat(data[currentCity].length) ) {
+            console.log(data[currentCity].street)
+            if ( colorIndex > colors.length - 1 ) { colorIndex = 0 }
+
+            var points = c.freeDrawingBrush._points.concat([]),
+                pathData = c.freeDrawingBrush.convertPointsToSVGPath(points).join(''),
+                path = new fabric.Path(pathData)
+
+            c.freeDrawingBrush._points = [new fabric.Point(0,0), new fabric.Point(0,0)]
+            c._onMouseUpInDrawingMode(ops.e)
+
+            c.clearContext(c.contextTop)
+            path.set({
+                fill : null,
+                stroke : colors[colorIndex],
+                strokeWidth : 5,
+                //strokeLineJoin : 'round',
+                strokeLineCap : 'round'
+            })
+            c.add(path)
+
+            colorIndex++
+            c.freeDrawingBrush.color = colors[colorIndex]
+            c._onMouseDownInDrawingMode(ops.e)
+
+            distance -= parseFloat(data[currentCity].length)/10
+            currentCity++
+        }
+    } catch(err) {
+        console.warn('caught error')
+        if (points.length < 2) {
+            console.warn('removing mouse handler')
+            c.off('mouse:move', mousemove)
+        }
     }
 }
 
@@ -23,6 +49,9 @@ function init() {
     c = new fabric.Canvas($('maps'), {
         isDrawingMode : true
     })
+
+    c.freeDrawingBrush.width = 5
+    c.freeDrawingBrush.color = colors[0]
 
     c.on('object:added', function(e) {
         if ( e.target.type === 'path' ) {
@@ -58,18 +87,24 @@ var canvasResize, c, data, paths = [], distance = 0,
     colorIndex = 0,
     clear = $('clear'),
     colors = [
-        "#4F7A38",
-        "#59E341",
-        "#C3EAAC",
-        "#B8DE70",
-        "#66E175",
-        "#4DAA6F",
-        "#56B435",
-        "#9DE13D",
-        "#96B372",
-        "#77E39C",
-        "#5E7D59",
-        "#56922D"
+        "#D054DC",
+        "#51AE33",
+        "#DB4D2C",
+        "#338C94",
+        "#A25177",
+        "#878327",
+        "#6F6FD8",
+        "#418C56",
+        "#6295CE",
+        "#CB872D",
+        "#C84F57",
+        "#B47EC3",
+        "#D850A4",
+        "#A0572C",
+        "#DB3776",
+        "#5C5F93",
+        "#9643A1",
+        "#568B2D"
     ]
 
 clear.addEventListener('click', function(e) { c.clear() })
