f5ad8e847eb4e46160c8b670d7715733a622df34
Brian Whitton <me@brianwhitton.com>
2014-11-15 14:47:40 -0500

    more performance tweaks

--- js/dist/main.concat.js
+++ js/dist/main.concat.js
@@ -23223,82 +23223,88 @@ fabric.util.object.extend(fabric.IText.prototype, /** @lends fabric.IText.protot
 
 
 /*global document,fabric*/
-
 function $(elementID) {
     return document.getElementById(elementID)
 }
 
-function mousedown(e) {
-    c.on('mouse:move', mousemove)
-}
-
-function mouseup(e) {
-    c.off('mouse:move', mousemove)
-}
-
-function mousemove(ops) {
-    var deltaX, deltaY, e = ops.e
-    if ( ops.e.type.match(/touch/) )  {
-        deltaX = e.changedTouches[0].screenX - e.targetTouches[0].screenX
-        deltaY = e.changedTouches[0].screenY - e.targetTouches[0].screenY
-    } else {
-        deltaX = e.movementX
-        deltaY = e.movementY
-    }
-
-    distance += Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2))
-
-    try {
-        if ( distance * 10 > data[currentCity].length ) {
-            console.log(data[currentCity].street)
-            if ( colorIndex > colors.length - 1 ) { colorIndex = 0 }
-
-            var points = c.freeDrawingBrush._points.concat([]),
-                pathData = c.freeDrawingBrush.convertPointsToSVGPath(points).join(''),
-                path = new fabric.Path(pathData)
-
-            // the brush needs some points or will throw an error
-            // give it two points with no delta and it will return early
-            c.freeDrawingBrush._points = [new fabric.Point(0,0), new fabric.Point(0,0)]
-            c._onMouseUpInDrawingMode(ops.e)
-            c.clearContext(c.contextTop)
-
-            path.set({
-                fill : null,
-                stroke : colors[colorIndex],
-                strokeWidth : 5,
-                strokeLineCap : 'round',
-                name : data[currentCity].street,
-                length : data[currentCity].length,
-                cid : data[currentCity].cid
-            })
-            c.add(path)
-
-            if ( currentCity === data.length - 1 ) {
-                c.isDrawingMode = false
-                c.off('mouse:move', mousemove)
-                c.off('mouse:down', mousedown)
-                c.off('mouse:up', mouseup)
-                c.getObjects().forEach(function(path) {
-                    path.selectable =  false
-                })
-                return 
-            } else {
-                colorIndex++
-                c.freeDrawingBrush.color = colors[colorIndex]
-                c._onMouseDownInDrawingMode(ops.e)
-
-                distance -= data[currentCity].length/10
-                currentCity++
-
-                table.rows[currentCity].style.color = colors[colorIndex]
-            }
+var events = {
+    'mouse:down' : function(e) {
+        c.on('mouse:move', events['mouse:move'])
+    },
+    'mouse:up' : function(e) {
+        c.off('mouse:move', events['mouse:move'])
+    },
+    'mouse:move' : function(ops) {
+        var deltaX, deltaY, e = ops.e
+        if ( ops.e.type.match(/touch/) )  {
+            deltaX = e.changedTouches[0].screenX - e.targetTouches[0].screenX
+            deltaY = e.changedTouches[0].screenY - e.targetTouches[0].screenY
+        } else {
+            deltaX = e.movementX
+            deltaY = e.movementY
         }
-    } catch(err) {
-        //console.warn('caught error')
-        if (points.length < 2) {
-            //console.warn('removing mouse handler')
-            c.off('mouse:move', mousemove)
+
+        distance += Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2))
+
+        try {
+            if ( distance * 10 > data[currentCity].length ) {
+                console.log(data[currentCity].street)
+                if ( colorIndex > colors.length - 1 ) { colorIndex = 0 }
+
+                // since we're still currently drawing, need to generate a path of the current
+                // undrawn points and add it manually to the canvas
+                var points = c.freeDrawingBrush._points.concat([]),
+                    pathData = c.freeDrawingBrush.convertPointsToSVGPath(points).join(''),
+                    path = new fabric.Path(pathData)
+
+                // the brush needs some points or will throw an error
+                // give it two points with no delta and it will return early
+                c.freeDrawingBrush._points = [new fabric.Point(0,0), new fabric.Point(0,0)]
+                c._onMouseUpInDrawingMode(ops.e)
+                c.clearContext(c.contextTop)
+
+                path.set({
+                    fill : null,
+                    stroke : colors[colorIndex],
+                    strokeWidth : 5,
+                    strokeLineCap : 'round',
+                    name : data[currentCity].street,
+                    length : data[currentCity].length,
+                    cid : data[currentCity].cid,
+                    // performance considerations
+                    hasControls : false,
+                    hasBorders : false,
+                    hasRotatingPoint : false,
+                    selectable : false
+                })
+                c.add(path)
+
+                if ( currentCity === data.length - 1 ) {
+                    c.isDrawingMode = false
+                    c.skipTargetFind = false
+                    c.clearEvts()
+                    $('toggleDrawing').setAttribute('disabled', true)
+                    //c.getObjects().forEach(function(path) {
+                    //    path.selectable =  false
+                    //})
+                    return 
+                } else {
+                    colorIndex++
+                    c.freeDrawingBrush.color = colors[colorIndex]
+                    c._onMouseDownInDrawingMode(ops.e)
+
+                    distance -= data[currentCity].length/10
+                    currentCity++
+
+                    table.rows[currentCity].style.color = colors[colorIndex]
+                }
+            }
+        } catch(err) {
+            //console.warn('caught error')
+            if (points.length < 2) {
+                //console.warn('removing mouse handler')
+                c.off('mouse:move', mousemove)
+            }
         }
     }
 }
@@ -23308,7 +23314,12 @@ function addPathData(ops) {
     path.set({
         name : data[currentCity].street,
         length : data[currentCity].length,
-        cid : data[currentCity].cid
+        cid : data[currentCity].cid,
+        // performance considerations
+        hasControls : false,
+        hasBorders : false,
+        hasRotatingPoint : false,
+        selectable : false
     })
 }
 
@@ -23331,19 +23342,36 @@ function init() {
         data.push(dataObj)
     })
 
+    fabric.Canvas.prototype.clearEvts = function() {
+        for ( var evt in events ) {
+            if ( events.hasOwnProperty(evt) ) {
+                this.off(evt, events[evt])
+            }
+        }
+    }
+
+    fabric.Canvas.prototype.toggleDrawing = function () {
+        this.skipTargetFind = !this.skipTargetFind
+        this.isDrawingMode = !this.isDrawingMode
+    }
+
+
     c = new fabric.Canvas($('maps'), {
         isDrawingMode : true,
         perPixelTargetFind : true,
-        targetFindTolerance : 25
+        targetFindTolerance : 25,
+        //renderOnAddRemove : false,
+        skipTargetFind : true
     })
 
     c.freeDrawingBrush.width = 5
     c.freeDrawingBrush.color = colors[0]
     table.rows[0].style.color = colors[0]
 
+    // this checks for paths added through the drawing mode mouse up event
     c.on('path:created', addPathData)
-    c.on('mouse:down', mousedown)
-    c.on('mouse:up', mouseup)
+    c.on('mouse:down', events['mouse:down'])
+    c.on('mouse:up', events['mouse:up'])
 
     c.on('mouse:move', function(ops) {
         var target = c.findTarget(ops.e),
@@ -23410,6 +23438,7 @@ var canvasResize, c, data = [], paths = [], distance = 0,
     colorIndex = 0,
     clear = $('clear'),
     save = $('download'),
+    drawingMode = $('toggleDrawing'),
     colors = _.shuffle([
         "#D054DC",
         "#51AE33",
@@ -23435,6 +23464,9 @@ clear.addEventListener('click', function(e) { c.clear() })
 save.addEventListener('click', function(e) {
     this.setAttribute('href', c.toDataURL())
     this.setAttribute('download', 'map')
+})
+drawingMode.addEventListener('change', function() {
+    c.toggleDrawing()
 })
 
 xhr.addEventListener('load', init)
