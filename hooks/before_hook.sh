7e25075e009079145e04ea01a09f07484124ebf1
Brian Whitton <me@brianwhitton.com>
2015-02-14 20:17:39 -0500

    installing normalize direclty

--- hooks/before_hook.sh
+++ hooks/before_hook.sh
@@ -7,12 +7,8 @@ if test -d $cache_dir/ruby/.gem; then
   cp -r $cache_dir/ruby/.gem $build_dir
   HOME=$build_dir gem update compass --user-install --no-rdoc --no-ri
 else
-  status "installing compass"
   HOME=$build_dir gem install compass --user-install --no-rdoc --no-ri
-  status "install bundler"
-  HOME=$build_dir gem install bundler --user-install --no-rdoc --no-ri
-  status "installing ruby deps"
-  bundle install
+  HOME=$build_dir gem install compass-normalize --user-install --no-rdoc --no-ri
 fi
 
 # Cache for Ruby
