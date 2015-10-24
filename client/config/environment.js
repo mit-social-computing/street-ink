491d3f2b6f588a0e02178abf44915e5bd2fea89f
Brian Whitton <me@brianwhitton.com>
2015-10-24 16:30:39 -0400

    adds api url to config

--- client/config/environment.js
+++ client/config/environment.js
@@ -50,7 +50,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.apiHost = process.env.API_URL
+    ENV.apiHost = 'http://52.0.85.232'
     ENV.apiPrefix = 'api/v1'
   }
 
